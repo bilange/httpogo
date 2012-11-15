e730d2f7d0123229cc0d67630ed3e109afdf65de
Eric Belanger <eric.belanger@ronasherbrooke.com>
2012-11-15 15:22:17 -0500

    Support Markdown: HTML template en fichier & Markdown source
    On peut maintenant voir le code source Markdown (fichier d'origine
    non parsed) en faisant une requette se terminant par ".md.txt".
    
    De plus, si on rajoute un fichier markdown-template.html dans le
    meme dossier que le binary serveur, ce template sera utilise pour
    l'affichage HTML du Markdown.

--- http.go
+++ http.go
@@ -1,3 +1,16 @@
+/*  
+
+ATTENTION: a *chaque* fois qu'on compile le programme et qu'on ecrase le
+fichier executable d'origine où le serveur sera exécuté, on DOIT ABSOLUMENT
+faire la commande suivante en tant que root:
+
+	setcap cap_net_bind_service=+ep /chemin/vers/fichier/compilé
+
+Ceci nous donne la possibilité d'ouvrir un port TCP < 1024 en tant qu'un
+usager UNIX non-root.
+
+*/
+
 package main
 
 import (
@@ -53,7 +66,7 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 
 	//Pour les fichiers non-existants 404.
 	fexists, _ := rona.FileExists(fileAbsolute)
-	if fexists == false {
+	if fexists == false && !strings.HasSuffix(r.URL.Path, ".md.txt") {
 		fileNotFoundHandler(w, r)
 		return
 	}
@@ -74,7 +87,10 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch {
 	case strings.HasSuffix(r.URL.Path, ".md"):
-		markdownHandler(w, r, fileAbsolute)
+		markdownHandler(w, r, fileAbsolute, false)
+		return
+	case strings.HasSuffix(r.URL.Path, ".md.txt"):
+		markdownHandler(w, r, fileAbsolute, true)
 		return
 	case mimeType == "application/octet-stream",
 		mimeType == "" && fexecutable == true,
@@ -122,148 +138,37 @@ func main() {
 	http.HandleFunc("/", requestHandler)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
-		println("Erreur de demarrage du serveur web: ", err.Error())
+		fmt.Printf("Erreur: %s\n", err.Error())
 	}
 	return
 }
 
-func markdownHandler(w http.ResponseWriter, req *http.Request, file string) {
-	header := `
-<!DOCTYPE html><html><head><meta charset="UTF-8"><style>html { font-size: 100%; overflow-y: scroll; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+func markdownHandler(w http.ResponseWriter, req *http.Request, file string, printSource bool) {
+	if printSource == false { // Markdown -> HTML
+		template, err := ioutil.ReadFile(filepath.Join(wwwRoot, "markdown-template.html"))
+		if err != nil {
+			template = []byte(`<!DOCTYPE html><html><head><meta charset="UTF-8"> </head> <body><!--BODY--> </body></html> `)
+		}
 
-body{
-  color:#444;
-  font-family:Georgia, Palatino, 'Palatino Linotype', Times,
-              'Times New Roman', serif,
-              "Hiragino Sans GB", "STXihei", "微软雅黑";
-  font-size:12px;
-  line-height:1.5em;
-  background:#fefefe;
-  width: 45em;
-  margin: 10px auto;
-  padding: 1em;
-  outline: 1300px solid #FAFAFA;
-}
+		md, err := ioutil.ReadFile(file)
+		if err == nil {
+			output := blackfriday.MarkdownCommon(md)
+			//w.Write([]byte(header))
+			//w.Write(output)
+			//w.Write([]byte(footer))
 
-a{ color: #0645ad; text-decoration:none;}
-a:visited{ color: #0b0080; }
-a:hover{ color: #06e; }
-a:active{ color:#faa700; }
-a:focus{ outline: thin dotted; }
-a:hover, a:active{ outline: 0; }
-
-span.backtick {
-  border:1px solid #EAEAEA;
-  border-radius:3px;
-  background:#F8F8F8;
-  padding:0 3px 0 3px;
-}
-
-::-moz-selection{background:rgba(255,255,0,0.3);color:#000}
-::selection{background:rgba(255,255,0,0.3);color:#000}
-
-a::-moz-selection{background:rgba(255,255,0,0.3);color:#0645ad}
-a::selection{background:rgba(255,255,0,0.3);color:#0645ad}
-
-p{
-margin:1em 0;
-}
-
-img{
-max-width:100%;
-}
-
-h1,h2,h3,h4,h5,h6{
-font-weight:normal;
-color:#111;
-line-height:1em;
-}
-h4,h5,h6{ font-weight: bold; }
-h1{ font-size:2.5em; }
-h2{ font-size:2em; border-bottom:1px solid silver; padding-bottom: 5px; }
-h3{ font-size:1.5em; }
-h4{ font-size:1.2em; }
-h5{ font-size:1em; }
-h6{ font-size:0.9em; }
-
-blockquote{
-color:#666666;
-margin:0;
-padding-left: 3em;
-border-left: 0.5em #EEE solid;
-}
-hr { display: block; height: 2px; border: 0; border-top: 1px solid #aaa;border-bottom: 1px solid #eee; margin: 1em 0; padding: 0; }
-
-
-pre , code, kbd, samp { 
-  color: #000; 
-  font-family: monospace; 
-  font-size: 0.88em; 
-  border-radius:3px;
-  background-color: #F8F8F8;
-  border: 1px solid #CCC; 
-}
-pre { white-space: pre; white-space: pre-wrap; word-wrap: break-word; padding: 5px;}
-pre code { border: 0px !important; }
-code { padding: 0 3px 0 3px; }
-
-b, strong { font-weight: bold; }
-
-dfn { font-style: italic; }
-
-ins { background: #ff9; color: #000; text-decoration: none; }
-
-mark { background: #ff0; color: #000; font-style: italic; font-weight: bold; }
-
-sub, sup { font-size: 75%; line-height: 0; position: relative; vertical-align: baseline; }
-sup { top: -0.5em; }
-sub { bottom: -0.25em; }
-
-ul, ol { margin: 1em 0; padding: 0 0 0 2em; }
-li p:last-child { margin:0 }
-dd { margin: 0 0 0 2em; }
-
-img { border: 0; -ms-interpolation-mode: bicubic; vertical-align: middle; }
-
-table { border-collapse: collapse; border-spacing: 0; }
-td { vertical-align: top; }
-
-@media only screen and (min-width: 480px) {
-body{font-size:14px;}
-}
-
-@media only screen and (min-width: 768px) {
-body{font-size:16px;}
-}
-
-@media print {
-  * { background: transparent !important; color: black !important; filter:none !important; -ms-filter: none !important; }
-  body{font-size:12pt; max-width:100%;}
-  a, a:visited { text-decoration: underline; }
-  hr { height: 1px; border:0; border-bottom:1px solid black; }
-  a[href]:after { content: " (" attr(href) ")"; }
-  abbr[title]:after { content: " (" attr(title) ")"; }
-  .ir a:after, a[href^="javascript:"]:after, a[href^="#"]:after { content: ""; }
-  pre, blockquote { border: 1px solid #999; padding-right: 1em; page-break-inside: avoid; }
-  tr, img { page-break-inside: avoid; }
-  img { max-width: 100% !important; }
-  @page :left { margin: 15mm 20mm 15mm 10mm; }
-  @page :right { margin: 15mm 10mm 15mm 20mm; }
-  p, h2, h3 { orphans: 3; widows: 3; }
-  h2, h3 { page-break-after: avoid; }
-}</style>
-`
-	footer := "</html>"
-
-	md, err := ioutil.ReadFile(file)
-	if err == nil {
-		output := blackfriday.MarkdownCommon(md)
-		w.Write([]byte(header))
-		w.Write(output)
-		w.Write([]byte(footer))
+			w.Write([]byte(strings.Replace(string(template), "<!--BODY-->", string(output), -1)))
+		}
+	} else {
+		md, err := ioutil.ReadFile(file[0 : len(file)-4])
+		if err == nil {
+			w.Write([]byte(md))
+		} else {
+			w.Write([]byte("Unable to read markdown file."))
+		}
 	}
-
 	return
+
 }
 
 //phpHandler se charge des scripts PHP, pour backward-compatibility.
